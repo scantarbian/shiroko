ee3c34a868067cbe172010e7685e73a2cc415059
scantarbian <davidpamereanb@gmail.com>
2023-06-02 15:09:53 +0700

    feat: working place lookup

--- app/playground/trafficsim/PlaceLookup.tsx
+++ app/playground/trafficsim/PlaceLookup.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Dijkstra, NodeInformation } from "./types";
+import { Dijkstra, NodeInformation, DijkstraCalculationData } from "./types";
 
 type Inputs = {
   origin: string;
@@ -12,6 +13,9 @@ type Props = {
 };
 
 const PlaceLookup = ({ nodeInformation, calculateDijkstra }: Props) => {
+  const [dijkstraData, setDijkstraData] =
+    useState<DijkstraCalculationData | null>(null);
+
   const {
     register,
     handleSubmit,
@@ -19,7 +23,27 @@ const PlaceLookup = ({ nodeInformation, calculateDijkstra }: Props) => {
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    // match origin with nodeInformation alias, get the key
+    const origin = nodeInformation.find(
+      (node) => node.alias === data.origin
+    )?.key;
+
+    console.log("origin", origin);
+
+    // match destination with nodeInformation alias, get the key
+    const destination = nodeInformation.find(
+      (node) => node.alias === data.destination
+    )?.key;
+
+    console.log("destination", destination);
+
+    // calculate dijkstra
+    if (origin !== undefined && destination !== undefined) {
+      const dijkstraData = calculateDijkstra(origin, destination);
+      setDijkstraData(dijkstraData);
+    } else {
+      console.error("Origin or destination not found");
+    }
   };
 
   console.log(watch("origin"));
@@ -46,7 +70,41 @@ const PlaceLookup = ({ nodeInformation, calculateDijkstra }: Props) => {
         </button>
       </form>
       <div>
-        <span>Result</span>
+        {dijkstraData && (
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <span>Result</span>
+              <span>
+                {dijkstraData.route
+                  .map((node) => {
+                    return `${nodeInformation[node].alias} (${nodeInformation[node].name})`;
+                  })
+                  .join(" -> ")}
+              </span>
+              <span>Weights: {dijkstraData.totalWeight}</span>
+            </div>
+            <div className=" grid grid-cols-3">
+              <span>NODE</span>
+              <span>W FR OG</span>
+              <span>PREV NODE</span>
+              {dijkstraData.summary.map((node, j) => {
+                return (
+                  <>
+                    <span key={`node-${j}`}>
+                      {nodeInformation[node.node].name}
+                    </span>
+                    <span key={`distance-${j}`}>{node.weight}</span>
+                    <span key={`previous-${j}`}>
+                      {node.weight === 0
+                        ? "N/A"
+                        : nodeInformation[node.previous].name}
+                    </span>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
